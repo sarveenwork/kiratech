c22e4beccd9a8c6e513d14330a16f3e1bd751a81
Sarveen <sarveen.work@gmail.com>
2025-04-26 17:35:00 +0800

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
-Vue.config.productionTip = false
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import '@/styles/custom.scss';
 
+import '@/mixins/helpers'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
